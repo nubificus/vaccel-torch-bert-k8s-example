afd10dbeda6d31a4f5d1e6874e36192aae6c23d6
Anastassios Nanos <ananos@nubificus.co.uk>
2025-07-13 21:41:54 +0000

    feat: Update dockerfiles and add prom exporter
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- exporter/log_exporter.py
+++ exporter/log_exporter.py
@@ -1,8 +1,16 @@
 from prometheus_client import start_http_server, Gauge
 import time
 import re
+import os
 
 DURATION_GAUGE = Gauge('vaccel_inference_duration_ms', 'Duration per inference line', ['line'])
+
+LOG_FILE = '/tmp/output.log'
+
+def wait_for_file(filepath):
+    while not os.path.exists(filepath):
+        print(f"Waiting for {filepath}...")
+        time.sleep(0.5)
 
 def tail_log(filepath):
     with open(filepath, 'r') as f:
@@ -19,5 +27,7 @@ def tail_log(filepath):
                 DURATION_GAUGE.labels(line=line_number).set(duration_ms)
 
 if __name__ == '__main__':
-    start_http_server(9100)  # Prometheus will scrape this port
-    tail_log('/tmp/output.log')
+    start_http_server(9100)
+    wait_for_file(LOG_FILE)
+    tail_log(LOG_FILE)
+
